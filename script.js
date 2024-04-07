2a662a27147df25cc78d4583e8f3db5ec7c37e88
Kai Yiu Ng Tony <19031798@student.curtin.edu.au>
2024-04-08 00:46:31 +0800

    add Text to speech

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+/* Lets keep score! */
+let win = 0;
+let loses = 0;
+let draws = 0;
+
+function say(theMessage){
+    let msg = new SpeechSynthesisUtterance(theMessage);
+    speechSynthesis.speak(msg);
+}
+
 /* This is a simplified version of the game.
 The Computer ALWAYS picks ROCK.  
 Does the user's choose win,lose, or tie vs ROCK? */
@@ -5,12 +15,20 @@ function play(user) {
     let result = "";
     if (user === 'rock') {
         result = "tie";
+        draws++;
     }
     if (user === 'paper') {
         result = "win";
+        win++;
     }
     if (user === 'scissors') {
         result = "lose";
+        loses++;
     }
-    alert("Computer chose rock, you " + result + "!");
+    let myMessage = "Computer chose rock, you " + result + "!" + 
+    "\n\n" + wins + "wins" +
+    "\n"   + loses + "losses" +
+    "\n"   + draws + "draws";
+    alert(myMessage)
+    say(myMessage)
 }
